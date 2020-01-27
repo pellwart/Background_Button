7d975cae99d5dabd069dc213bbfd2199873f717f
pellwart <56221429+pellwart@users.noreply.github.com>
2020-01-27 23:16:11 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let btn = document.getElementById('switch');
+
+
+
+
+//Creates random number
+function getRandom() {
+
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+
+}
+
+//Changes background color
+function changeBackground() {
+
+    var background = document.getElementsByTagName('body')[0];
+    background.style.background = getRandom();
+
+}
+
+
+
+
+btn.addEventListener('click', () => {
+
+    alert('hello');
+
+    changeBackground();
+    
+
+});
+
+
